a3fc682e6ee38454152aa3e90367aacf8c5a1d3e
chimolirgb <chimolilucy@gmail.com>
2021-05-24 10:27:46 +0300

    modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { AppHighlightDirective } from './app-highlight.directive';
 import { SearchComponent } from './search/search.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     HomeComponent,
     UserDetailComponent,
     AppHighlightDirective,
     SearchComponent,
+ 
     SearchFormComponent
   ],
   imports: [

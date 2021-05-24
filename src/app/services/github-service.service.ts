dd5b55b51350c9cafad56f943d401b436e1656a8
chimolirgb <chimolilucy@gmail.com>
2021-05-24 09:17:14 +0300

    src/app/services

---
+++ src/app/services/github-service.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+import { User } from '../classes/user';
+import { Repository } from '../classes/repository';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  usersFound: User[] = [];
+
+  reposFound: Repository[] = [];
+
+  searchUsers(term: string) {
+    let endpoint = `https://api.github.com/search/users?access_token=${environment.apiKey}&q=${term}&per_page=10`
+    // let endpoint = "https://api.github.com/search/users?access_token="+environment.apiKey + "&q=" + term;
+    let promise = new Promise<void>((resolve, reject) => {
+      this.client
+        .get(endpoint)
+        .toPromise()
+        .then(
+          (results: any) => {
+            this.usersFound = [];
+            for (let i = 0; i < results['items'].length; i++) {
+              let name = results['items'][i]['login'];
+              let imagePath = results['items'][i]['avatar_url'];
+              let user = new User(name, imagePath);
+              this.usersFound.push(user);
+            }
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
+
+  searchRepos(term: string) {
+    let endpoint = `https://api.github.com/search/repositories?access_token=${environment.apiKey}&q=${term}&per_page=7`
+    // let endpoint = "https://api.github.com/search/users?access_token="+environment.apiKey + "&q=" + term;
+    let promise = new Promise<void>((resolve, reject) => {
+      this.client
+        .get(endpoint)
+        .toPromise()
+        .then(
+          (results: any) => {
+            this.reposFound = [];
+            for (let i = 0; i < results['items'].length; i++) {
+              let name = results['items'][i]['full_name'];
+              let repoUrl = results['items'][i]['html_url'];
+              let userName = results['items'][i]['owner']['login'];
+              let userImage = results['items'][i]['owner']['avatar_url'];
+              let repo = new Repository(name, repoUrl, userName, userImage);
+              this.reposFound.push(repo);
+            }
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
+
+
+
+  constructor(private client: HttpClient) { }
+}
